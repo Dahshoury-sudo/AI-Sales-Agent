0908736caddae448b90e0fe15d6afef67586697c
Fo2sh <mfb4010@gmail.com>
2026-07-27 17:36:29 +0300

    fix ambigous messages now he doesn't say i don't find a perfume for you

--- perfume_ai/test_ambiguous.py
+++ perfume_ai/test_ambiguous.py
@@ -3,6 +3,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfume_ai.settings')
 django.setup()
 
 from products.services.router import route
-msg = 'قارنلي بين marino و Acqua di Gio'
-res, _ = route(msg)
-with open('res_compare.txt', 'w', encoding='utf-8') as f: f.write(res)
+res, _ = route('شسيشسيشسي')
+with open('res_ambiguous.txt', 'w', encoding='utf-8') as f:
+    f.write(res)
